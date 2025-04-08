16003856478c7e7a5e76485ec72bc698f02498aa
ErikrafT <139592038+erikraft@users.noreply.github.com>
2025-04-07 23:28:41 -0300

    Update ui.js

--- public/scripts/ui.js
+++ public/scripts/ui.js
@@ -747,7 +747,7 @@ class Dialog {
             document.activeElement.blur();
             window.blur();
         }
-        document.title = 'PairDrop | Transfer Files Cross-Platform. No Setup, No Signup.';
+        document.title = '📤｜ErikrafT Drop｜Transfer Files!!!';
         changeFavicon("images/favicon-96x96.png");
         this.correspondingPeerId = undefined;
     }
